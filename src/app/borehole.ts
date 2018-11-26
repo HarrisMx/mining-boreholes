1e44b4a2c01bffb86d5a51d934b4eb817d564506
Mxolisi <mxolisi.ngwenya@moreismore.global>
2018-11-26 11:31:57 +0200

    Added Project files

---
+++ src/app/borehole.ts
@@ -0,0 +1,45 @@
+export class Borehole {
+    private name: string;
+    private type: any;
+    private latitude: number;
+    private longitude: number;
+    private elevation: number;
+
+    public constructor(name: string, type: any, lat: number, long: number, elev: number) {
+        this.name = name;
+        this.type = type;
+        this.latitude = lat;
+        this.longitude = long;
+        this.elevation = elev;
+    }
+
+    public setLatitude() {
+    }
+
+    public setLongitude() {
+    }
+
+    public setElevation() {
+    }
+
+    public setName() {
+    }
+
+    public setType() {
+    }
+
+    public getLatitude() {
+    }
+
+    public getLongitude() {
+    }
+
+    public getElevation() {
+    }
+
+    public getName() {
+    }
+
+    public getType() {
+    }
+}
---
+++ src/app/water-levels.ts
@@ -0,0 +1,10 @@
+import { Borehole } from './borehole';
+
+export class WaterLevels extends Borehole {
+    private date: any;
+    private reading: number;
+
+    public constructor() {
+        super();
+    }
+}
