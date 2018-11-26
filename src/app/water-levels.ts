055138d7d85b6d31f23713c717bd9272e17d0cfd
Mxolisi <mxolisi.ngwenya@moreismore.global>
2018-11-26 11:48:40 +0200

    Added node-sass dependancies to fix errors

--- src/app/water-levels.ts
+++ src/app/water-levels.ts
@@ -3,8 +3,4 @@ import { Borehole } from './borehole';
 export class WaterLevels extends Borehole {
     private date: any;
     private reading: number;
-
-    public constructor() {
-        super();
-    }
 }
