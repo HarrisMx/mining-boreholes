20dc1c27d174060ccb63fcb57c9f16e89434bcfa
Mxolisi <mxolisi.ngwenya@moreismore.global>
2018-12-11 13:36:46 +0200

    Added class for water levels

--- src/app/water-levels.ts
+++ src/app/water-levels.ts
@@ -1,6 +1,18 @@
 import { Borehole } from './borehole';
 
 export class WaterLevels extends Borehole {
-    private date: any;
-    private reading: number;
+    private _date: any;
+    public get date(): any {
+        return this._date;
+    }
+    public set date(value: any) {
+        this._date = value;
+    }
+    private _reading: number;
+    public get reading(): number {
+        return this._reading;
+    }
+    public set reading(value: number) {
+        this._reading = value;
+    }
 }
